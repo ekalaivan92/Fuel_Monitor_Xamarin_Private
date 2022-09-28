c9e6a549148ba9ea77e7b40bcac6146c5d4162cc
ekalaivan <ekalaivan35@gmail.com>
2022-09-28 16:39:54 +0530

    feat: add tank capacity

--- FuelMonitor/Fragments/EstimationFragment.cs
+++ FuelMonitor/Fragments/EstimationFragment.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.OS;
+using Android.Preferences;
 using Android.Support.Design.Widget;
 using Android.Views;
 using Android.Widget;
@@ -16,6 +17,7 @@ namespace FuelMonitor.Fragments
         private Button calculateButton;
         private TextView expectedTravelDistanceValue;
         private TextView expectedFuelAvailabilityValue;
+        private TextInputEditText tankCapacityTextInput;
 
         public override void OnCreate(Bundle savedInstanceState)
         {
@@ -32,8 +34,11 @@ namespace FuelMonitor.Fragments
             calculateButton = view.FindViewById<Button>(Resource.Id.calculateButton);
             expectedTravelDistanceValue = view.FindViewById<TextView>(Resource.Id.expectedTravelDistanceValue);
             expectedFuelAvailabilityValue = view.FindViewById<TextView>(Resource.Id.expectedFuelAvailabilityValue);
+            tankCapacityTextInput = view.FindViewById<TextInputEditText>(Resource.Id.tankCapacitInLitTextInput);
 
             calculateButton.Click += Caculate;
+
+            lastODOValueTextInput.RequestFocus();
 
             LoadInitialValues();
 
@@ -45,13 +50,15 @@ namespace FuelMonitor.Fragments
             var isOdoValid = decimal.TryParse(lastODOValueTextInput.Text, out decimal odoValue);
             var isAvgMilageValid = decimal.TryParse(avgMilageTextInput.Text, out decimal avgMilage);
             var isCurrentOdoValid = decimal.TryParse(currentODOValueTextInput.Text, out decimal currentODO);
+            var isTankCapacityValid = decimal.TryParse(tankCapacityTextInput.Text, out decimal tankCapacity);
 
             isOdoValid &= (odoValue >= 0);
             isAvgMilageValid &= (avgMilage > 0);
             isCurrentOdoValid &= (currentODO >= 0);
+            isTankCapacityValid &= (tankCapacity > 0);
             var isODODiffValid = currentODO >= odoValue;
 
-            var isValid = (isOdoValid && isAvgMilageValid && isCurrentOdoValid && isODODiffValid);
+            var isValid = (isOdoValid && isAvgMilageValid && isCurrentOdoValid && isODODiffValid && isTankCapacityValid);
 
             expectedTravelDistanceValue.Text = "0.00 km";
             expectedFuelAvailabilityValue.Text = "0.00 lt";
@@ -66,6 +73,10 @@ namespace FuelMonitor.Fragments
                 else if (!isAvgMilageValid)
                 {
                     message = "Invalid AVG millage";
+                }
+                else if (!isTankCapacityValid)
+                {
+                    message = "Invalid Tank capacity";
                 }
                 else if (!isCurrentOdoValid)
                 {
@@ -84,23 +95,39 @@ namespace FuelMonitor.Fragments
 
             if (isValid)
             {
-                var fullExpectedKM = 10 * avgMilage;
+                var fullExpectedKM = tankCapacity * avgMilage;
                 var expectedDistanceInKM = Math.Round(fullExpectedKM - (currentODO - odoValue));
                 var expectedFuelInLt = Math.Round(expectedDistanceInKM / avgMilage, 2);
 
-                expectedTravelDistanceValue.Text = expectedDistanceInKM.ToString("~ #0.00 km");
-                expectedFuelAvailabilityValue.Text = expectedFuelInLt.ToString("~ #0.00 lt");
+                if (expectedFuelInLt < 0 || expectedDistanceInKM < 0)
+                {
+                    var message = "Unexpected availability, Check input before calculate";
+                    var toast = Toast.MakeText(Context, message, ToastLength.Long);
+                    toast.Show();
+                }
+                else
+                {
+                    expectedTravelDistanceValue.Text = expectedDistanceInKM.ToString("~ #0.00 km");
+                    expectedFuelAvailabilityValue.Text = expectedFuelInLt.ToString("~ #0.00 lt");
+                }
             }
         }
 
         private void LoadInitialValues()
         {
+            var sharedPreference = PreferenceManager.GetDefaultSharedPreferences(Context);
+            var tankCapacity = (decimal)sharedPreference.GetFloat("TankCapacity", 10.00f);
+
+            tankCapacityTextInput.Text = tankCapacity.ToString("#0.00");
+
             if (FuelFillDAO.HasInfoToEstimste())
             {
                 var lastFuelFilling = FuelFillDAO.GetLatestFillingSummary();
 
                 lastODOValueTextInput.Text = lastFuelFilling.ODOValue?.ToString("#0.00");
                 avgMilageTextInput.Text = lastFuelFilling.AVGKMPL.ToString("#0.00");
+
+                currentODOValueTextInput.RequestFocus();
             }
         }
     }
